a015c2c7fec31699210427d3a32797a1024c9706
Matheus Roxo Brandão <matheus.roxo.brandao31@gmail.com>
2020-12-23 01:00:37 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AulaPOO_PolimosrfismoSobrecarga.classes;
+
+namespace AulaPOO_PolimosrfismoSobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Funcionario fun = new Funcionario(); 
+            fun.Mostrar();
+            fun.Mostrar("Paulo");
+            fun.Mostrar(2);
+
+            Console.Beep(900, 5000);
+        }
+    }
+}
